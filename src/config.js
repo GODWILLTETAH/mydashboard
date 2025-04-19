206e31a4d4b3019581fd50818fa36fdc77d64bb1
Godwill Tetah <godwilltetah@gmail.com>
2025-04-19 08:46:01 +0100

    Auto commit from Ansible

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = 'http://89.116.24.202:2037/api/v1';
+const API_BASE_URL = 'https://dialect-api.go237.com/api/v1';
+const MEDIA_BASE_URL = 'https://dialect-api.go237.com';
 
-export default API_BASE_URL;
+export default { API_BASE_URL, MEDIA_BASE_URL };
